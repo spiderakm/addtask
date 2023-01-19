2d2032f4394e6d2c859d83faa88974773f132c5a
ashok kumar <spider.akm@gmail.com>
2023-01-19 09:27:39 +0530

    my first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// console.log(document.forms)
+// console.log(document.all)
+// console.log(document.domain)
+// console.log(document.images)
+// console.log(document.getElementById('header-title'))
+// var h = document.getElementById('header-title')
+// h.textContent = "hello"
+// h.innerText = 'hiii'
+// console.log(h.innerText)
+// h.innerText = "hello'
+// h.style.borderBottom='solid 3px #000'
+
+// var h1 = document.getElementById('main-header')
+
+// h1.style.borderBottom='solid 3px #000'
+
+// add item task
+
+var additem = document.querySelector('h2')
+additem.style.color='green'
